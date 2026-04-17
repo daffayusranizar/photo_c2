ad0bf8afd7ccfa143173e98b00396755c24c2250
Daffa Yusranizar Arrifi <daffa28.dn@gmail.com>
2026-04-17 14:52:28 +0800

    Initial Commit

---
+++ Photos_C2/Photos_C2App.swift
@@ -0,0 +1,17 @@
+//
+//  Photos_C2App.swift
+//  Photos_C2
+//
+//  Created by Daffa Yuranizar Arrifi on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Photos_C2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
